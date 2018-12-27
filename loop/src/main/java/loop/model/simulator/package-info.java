458a273fc8319f591fef9cdaaf97e0a6eef8dc50
Christian <sw_development@web.de>
2018-12-27 18:21:41 +0100

    Simulator Package hinzugefügt

---
+++ loop/src/main/java/loop/model/simulator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the Simulator , which is the models interface for starting and stop-
+ * ping simulations.
+ */
+/**
+ * @author christian
+ *
+ */
+package loop.model.simulator;
