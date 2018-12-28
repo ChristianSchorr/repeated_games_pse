97b84e12ff65a1eee2317021cb9807fa54c3c894
Pierre Toussing <pierre@toussing.lu>
2018-12-28 12:23:00 +0100

    Create package-info.java

---
+++ loop/src/main/java/loop/model/plugin/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the plugin system which can be used to integrate own implementations of the pair builder, success quantifier, strategy adjuster, equilibrium criterion or
+ * discrete distribution. 
+ */
+/**
+ * @author Pierre Toussing
+ *
+ */
+package loop.model.plugin;
