c2182936bffb0ceed49231979eeedb322d23e43b
Pierre Toussing <pierre@toussing.lu>
2018-12-30 10:56:16 +0100

    Update PluginLoader.java

--- loop/src/main/java/loop/model/plugin/PluginLoader.java
+++ loop/src/main/java/loop/model/plugin/PluginLoader.java
@@ -15,15 +15,35 @@ import java.util.ServiceLoader;
 public class PluginLoader {
 
 	/**
-	 * Loads all available plugins of the given type T.
+	 * Loads all available plugins.
 	 * @return a list of the loaded plugins
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	static <T> List<T> loadPlugins() {
 		ServiceLoader<Plugin> loader = ServiceLoader.load(Plugin.class);
 		ArrayList<T> plugins = new ArrayList<T>();
+		
 		while (loader.iterator().hasNext()) {
 			plugins.add((T) loader.iterator().next());
+		}
+		return plugins;		
+	}
+	
+	/**
+	 * Loads all available plugins of the given type T
+	 * @param t the type of the plugins that shall be loaded
+	 * @return  a list of the loaded plugins
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	static <T> List<T> loadPlugins(T t) {
+		ServiceLoader<Plugin> loader = ServiceLoader.load(Plugin.class);
+		ArrayList<T> plugins = new ArrayList<T>();
+		
+		while (loader.iterator().hasNext()) {
+			Object o = loader.iterator().next();
+			if (o.getClass().equals(t.getClass())) {			//checks if it is the correct type
+				plugins.add((T) o);
+			}
 		}
 		return plugins;		
 	}
