fe47efd06c2cf2e33ce5d840b30c5aa01932669f
Christian <sw_development@web.de>
2019-01-04 19:05:09 +0100

    stumps hinzugefügt

---
+++ loop/src/main/java/loop/model/Group.java
@@ -0,0 +1,17 @@
+package loop.model;
+
+public class Group implements Nameable {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ loop/src/main/java/loop/model/plugin/Plugin.java
@@ -0,0 +1,7 @@
+package loop.model.plugin;
+
+import loop.model.Nameable;
+
+public interface Plugin<T>  extends Nameable {
+
+}
