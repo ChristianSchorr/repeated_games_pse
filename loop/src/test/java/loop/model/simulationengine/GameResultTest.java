17ddfdf631e02eaaa4393ec95a56e4de46e86ac6
Sebastian <sebastian.feurer@online.de>
2019-01-16 21:39:21 +0100

    IterationResultTest und GameResultTest hinzugefügt

---
+++ loop/src/test/java/loop/model/simulationengine/GameResultTest.java
@@ -0,0 +1,49 @@
+package loop.model.simulationengine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameResultTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGameResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasAgent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPayoff() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasCooperated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAgents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOtherAgent() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ loop/src/test/java/loop/model/simulationengine/IterationResultTest.java
@@ -0,0 +1,49 @@
+package loop.model.simulationengine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IterationResultTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIterationResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAgents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquilibriumReached() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEfficiency() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAdapts() {
+		fail("Not yet implemented");
+	}
+
+}
