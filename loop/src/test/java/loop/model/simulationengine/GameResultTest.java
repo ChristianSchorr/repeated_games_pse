9f3d05623dcec2ff7d0e0b63a4512580400c64de
Sebastian <sebastian.feurer@online.de>
2019-01-16 21:52:07 +0100

    GameResult weitergearbeitet

--- loop/src/test/java/loop/model/simulationengine/GameResultTest.java
+++ loop/src/test/java/loop/model/simulationengine/GameResultTest.java
@@ -6,10 +6,27 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class GameResultTest {
+import loop.model.simulationengine.strategies.PureStrategy;
+import loop.model.simulationengine.strategies.Strategy;
 
+public class GameResultTest {
+	GameResult gameresult;
+	Strategy titForTat;
+	Strategy grim;
+	Agent agent1;
+	Agent agent2;
+
+	/**
+	 * Initialize the gameResult
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
+		titForTat = PureStrategy.titForTat();
+		grim = PureStrategy.grim();
+		agent1 = new Agent(20, titForTat, 2);
+		agent2 = new Agent(-10, grim, 0);
+		gameresult = new GameResult(agent1, agent2, true, true, 3, 3);
 	}
 
 	@After
