9135b4a5a07f4805d29ceee8415afec3eddbe51f
Sebastian <sebastian.feurer@online.de>
2019-01-17 14:51:52 +0100

    GameResultTest implementiert

--- loop/src/test/java/loop/model/simulationengine/GameResultTest.java
+++ loop/src/test/java/loop/model/simulationengine/GameResultTest.java
@@ -2,6 +2,8 @@ package loop.model.simulationengine;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,34 +35,90 @@ public class GameResultTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Tests the constructor of the class GameResult and the initialized attributs
+	 */
 	@Test
 	public void testGameResult() {
-		fail("Not yet implemented");
+		Strategy grouptitForTat = PureStrategy.groupTitForTat();
+		Strategy neverCooperate = PureStrategy.neverCooperate();
+		Agent testAgent1 = new Agent(50, grouptitForTat, 5);
+		Agent testAgent2 = new Agent(42, neverCooperate, 3);
+		GameResult testGameresult = new GameResult(testAgent1, testAgent2, true, false, 5, -2);
+		
+		assertTrue(testGameresult.hasAgent(testAgent1));
+		assertTrue(testGameresult.hasAgent(testAgent2));
+		assertEquals(5, testGameresult.getPayoff(testAgent1));
+		assertEquals(-2, testGameresult.getPayoff(testAgent2));
+		assertTrue(testGameresult.hasCooperated(testAgent1));
+		assertFalse(testGameresult.hasCooperated(testAgent2));
+		List<Agent> agents = testGameresult.getAgents();
+		assertTrue(testAgent1.equals(agents.get(0)));
+		assertTrue(testAgent2.equals(agents.get(1)));
+		assertTrue(testAgent2.equals(testGameresult.getOtherAgent(testAgent1)));
+		assertTrue(testAgent1.equals(testGameresult.getOtherAgent(testAgent2)));
 	}
 
+	/**
+	 * Tests the implementation of the method hasAgent
+	 */
 	@Test
 	public void testHasAgent() {
-		fail("Not yet implemented");
+		assertTrue(gameresult.hasAgent(agent1));
+		assertTrue(gameresult.hasAgent(agent2));
+		Agent agent3 = agent1.getCopy();
+		Agent agent4 = new Agent(0, titForTat, 1);
+		assertFalse(gameresult.hasAgent(agent3));
+		assertFalse(gameresult.hasAgent(agent4));
 	}
 
+	/**
+	 * Tests the implementation of the method getPayoff
+	 */
 	@Test
 	public void testGetPayoff() {
-		fail("Not yet implemented");
+		assertEquals(3, gameresult.getPayoff(agent1));
+		assertEquals(3, gameresult.getPayoff(agent2));
+		Agent agent3 = agent1.getCopy();
+		Agent agent4 = new Agent(0, titForTat, 1);
+		assertEquals(0, gameresult.getPayoff(agent3));
+		assertEquals(0, gameresult.getPayoff(agent4));
 	}
 
+	/**
+	 * Tests the implementation of the method hasCooperated
+	 */
 	@Test
 	public void testHasCooperated() {
-		fail("Not yet implemented");
+		assertTrue(gameresult.hasCooperated(agent1));
+		assertTrue(gameresult.hasCooperated(agent2));
+		Agent agent3 = agent1.getCopy();
+		Agent agent4 = new Agent(0, titForTat, 1);
+		assertFalse(gameresult.hasCooperated(agent3));
+		assertFalse(gameresult.hasCooperated(agent4));
 	}
 
+	/**
+	 * Tests the implementation of the method getAgents
+	 */
 	@Test
 	public void testGetAgents() {
-		fail("Not yet implemented");
+		List<Agent> agents = gameresult.getAgents();
+		assertTrue(agent1.equals(agents.get(0)));
+		assertTrue(agent2.equals(agents.get(1)));
 	}
 
+	/**
+	 * Tests the implementation of the method getOtherAgent
+	 */
 	@Test
 	public void testGetOtherAgent() {
-		fail("Not yet implemented");
+		assertTrue(agent2.equals(gameresult.getOtherAgent(agent1)));
+		assertTrue(agent1.equals(gameresult.getOtherAgent(agent2)));
+		Agent agent3 = agent1.getCopy();
+		Agent agent4 = new Agent(0, titForTat, 1);
+		assertNull(gameresult.getOtherAgent(agent3));
+		assertNull(gameresult.getOtherAgent(agent4));
 	}
 
 }
